72f36e7033e43f1f228e5ef240c5fc1168585b3b
Noboru Hanaki <noboruhanaki@gmail.com>
2018-11-07 08:33:35 +0000

    Something

--- app/controllers/fesus_controller.rb
+++ app/controllers/fesus_controller.rb
@@ -1,9 +1,22 @@
 class FesusController < ApplicationController
   def index
-    @fesus = Fesu.all
+    if params[:q] && params[:q].reject { |key, value| value.blank? }.present?
+      return_value = search_params
+      @q = Fesu.ransack(return_value, activated_true: true)
+      @title = "Search Result"
+    else
+      @q = Fesu.ransack(activated_true: true)
+      @title = "All users"
+    end
+    @fesus = @q.result.paginate(page: params[:page])
   end
 
   def show
     @fesus = Fesu.find_by(id: params[:id])
   end
+  
+  private
+    def search_params
+      params.require(:q).permit(:place_cont)
+    end
 end
