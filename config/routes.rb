cf667ba30deb569430c14b26b2f083fabea9d90e
Noboru Hanaki <noboruhanaki@gmail.com>
2018-11-27 16:41:49 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root "products#index"
-  get  "fesus/index" => "fesus#index"
-  get  "fesus/:id" => "fesus#show"
-  get  "products/search" => "products#search"
-  get  'static_pages/help'
-  get  'products/search'
+  get  "/fesus/index" => "fesus#index"
+  get  "/fesus/:id" => "fesus#show"
+  get  "/products/search" => "products#search"
+  get  '/static_pages/help'
+  get  '/static_pages/home'
+  get  '/products/search'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :users
 end
