9522943ea8811fd155a7e782b3892dca94d4bed0
Noboru Hanaki <noboruhanaki@gmail.com>
2018-11-28 03:52:53 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  get "sessions/new"
+  get "users/new"
   root "products#index"
   get  "/fesus/index" => "fesus#index"
   get  "/fesus/:id" => "fesus#show"
   get  "/products/search" => "products#search"
-  get  '/static_pages/help'
-  get  '/static_pages/home'
-  get  '/products/search'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  get  "/static_pages/help" => "static_pages#help"
+  get  "/static_pages/home" => "static_pages#home"
+  get  "/about" => "static_pages#about"
+  get  "/signup" => "users#new"
+  post "/signup" => "users#create"
+  get    "/login" => "sessions#new"
+  post   "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
   resources :users
 end
